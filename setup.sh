811526a1247038b37ad4545a8359e23923e50773
Ricky Waldt <rickywaldt@gmail.com>
2026-05-13 20:55:54 +0000

    fix: add missing flag

--- setup.sh
+++ setup.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-export PATH="$HOME/.local/share/chezmoi/bin:$PATH"
-
 if ! command -v chezmoi > /dev/null; then
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:rickywaldt/dotfiles.git
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin init --apply git@github.com:rickywaldt/dotfiles.git
 fi
 
 exit 0
