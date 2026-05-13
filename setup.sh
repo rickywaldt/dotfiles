9f2d0fdf023aa173485ebe948395867782709557
Ricky Waldt <rickywaldt@gmail.com>
2026-05-13 20:41:53 +0000

    docs: update setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+export PATH="$HOME/.local/bin:$PATH"
+
 if ! command -v chezmoi > /dev/null; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:rickywaldt/dotfiles.git
 fi
