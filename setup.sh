c67b798bf55deb3e2e184dbae175eb4b79ec77c1
Ricky Waldt <rickywaldt@gmail.com>
2026-05-13 20:48:49 +0000

    fix: incorrect path

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/share/chezmoi/bin:$PATH"
 
 if ! command -v chezmoi > /dev/null; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:rickywaldt/dotfiles.git
