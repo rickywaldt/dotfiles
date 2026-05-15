e9768d630f33355c78db359ca759b08c5e12fb76
Ricky Waldt <rickywaldt@gmail.com>
2026-05-15 16:31:46 +0200

    chore: update color scheme for neovim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,40 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
+		"shaunsingh/nord.nvim",
+		lazy = false,
 		priority = 1000,
-		config = true,
-		opts = {},
+
+		init = function()
+			-- Nord config
+			vim.g.nord_contrast = true
+			vim.g.nord_borders = false
+			vim.g.nord_disable_background = false
+			vim.g.nord_cursorline_transparent = false
+			vim.g.nord_enable_sidebar_background = true
+			vim.g.nord_italic = true
+			vim.g.nord_uniform_diff_background = true
+			vim.g.nord_bold = true
+		end,
+
+		config = function()
+			require("nord").set()
+		end,
 	},
 
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "nord",
+		},
+	},
+
+	-- Optional: make lualine match Nord
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = {
+			options = {
+				theme = "nord",
+			},
 		},
 	},
 }
