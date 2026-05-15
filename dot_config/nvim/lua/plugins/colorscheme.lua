3575a391f2e0ba017e9c570e6eb7e40f45d1c2d0
Ricky Waldt <rickywaldt@gmail.com>
2026-05-15 14:45:40 +0000

    chore: update colorscheme for nvim

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,39 +1,22 @@
 return {
 	{
-		"shaunsingh/nord.nvim",
+		"AlexvZyl/nordic.nvim",
 		lazy = false,
 		priority = 1000,
-
-		init = function()
-			-- Nord config
-			vim.g.nord_contrast = true
-			vim.g.nord_borders = false
-			vim.g.nord_disable_background = false
-			vim.g.nord_cursorline_transparent = false
-			vim.g.nord_enable_sidebar_background = true
-			vim.g.nord_italic = true
-			vim.g.nord_uniform_diff_background = true
-			vim.g.nord_bold = true
-		end,
-
-		config = function()
-			require("nord").set()
-		end,
 	},
 
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "nord",
+			colorscheme = "nordic",
 		},
 	},
 
-	-- Optional: make lualine match Nord
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			options = {
-				theme = "nord",
+				theme = "nordic",
 			},
 		},
 	},
