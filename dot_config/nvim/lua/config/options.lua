d33c822bd4b2822efe3e439612998f386beb15f7
Ricky Waldt <rickywaldt@gmail.com>
2026-05-15 14:55:39 +0000

    chore: update nvim config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,19 @@
 vim.g.snacks_animate = false
 vim.g.lazyvim_check_order = false
 
-vim.opt.ignorecase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-vim.opt.background = "dark"
-vim.opt.scrolloff = 8
+vim.o.background = "dark"
+vim.o.scrolloff = 8
+
+vim.o.clipboard = "unnamedplus"
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.mouse = "a"
+vim.o.autoindent = true
+
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
