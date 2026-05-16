67bb8d425a12bf1ed7ca75abcf5b8e65bff09715
Ricky Waldt <rickywaldt@gmail.com>
2026-05-16 16:44:57 +0200

    chore: fix option arrow in wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,6 @@
 local wezterm = require("wezterm")
 
-config = wezterm.config_builder()
-
-config = {
+local config = {
 	automatically_reload_config = true,
 	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
@@ -11,11 +9,25 @@ config = {
 	color_scheme = "Nord (Gogh)",
 	font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
 	font_size = 14.5,
+
 	window_padding = {
 		left = 8,
 		right = 8,
 		top = 4,
 		bottom = 4,
+	},
+
+	keys = {
+		{
+			key = "LeftArrow",
+			mods = "OPT",
+			action = wezterm.action.SendString("\x1bb"),
+		},
+		{
+			key = "RightArrow",
+			mods = "OPT",
+			action = wezterm.action.SendString("\x1bf"),
+		},
 	},
 }
 
