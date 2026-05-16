469d6559f2b0989e20cab9a514d5b262b083807b
Ricky Waldt <rickywaldt@gmail.com>
2026-05-16 16:38:50 +0200

    chore: disable mouse support in nvim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -8,9 +8,9 @@ vim.o.background = "dark"
 vim.o.scrolloff = 8
 
 vim.o.clipboard = "unnamedplus"
+vim.o.mouse = ""
 vim.o.wrap = false
 vim.o.linebreak = true
-vim.o.mouse = "a"
 vim.o.autoindent = true
 
 vim.o.shiftwidth = 2
